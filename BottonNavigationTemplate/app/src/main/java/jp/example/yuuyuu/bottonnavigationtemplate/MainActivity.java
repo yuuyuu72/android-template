6c84a8a5e7cf1097bc4287f9e8d5d4952bc2cfb0
yuuyuu <yuuyuu329@gmail.com>
2019-01-19 01:41:15 +0900

    first commit

---
+++ BottonNavigationTemplate/app/src/main/java/jp/example/yuuyuu/bottonnavigationtemplate/MainActivity.java
@@ -0,0 +1,54 @@
+package jp.example.yuuyuu.bottonnavigationtemplate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.frameLayout, new Fragment1())
+                            .commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.frameLayout, new Fragment2())
+                            .commit();
+                    return true;
+                case R.id.navigation_notifications:
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.frameLayout, new Fragment3())
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //初期設定Fragment
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, new Fragment1())
+                .commit();
+    }
+
+}
